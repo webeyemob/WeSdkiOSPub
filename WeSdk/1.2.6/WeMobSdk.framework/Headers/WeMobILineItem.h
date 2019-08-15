0357d32cbb4ad77b1fe497b34de7632122467d27
hang.wang <847972062@qq.com>
2019-08-15 09:36:59 +0800

    .

---
+++ WeSdk/1.2.6/WeMobSdk.framework/Headers/WeMobILineItem.h
@@ -0,0 +1,24 @@
+//
+//  WeMobILinItem.h
+//  Created by Matthew on 2019/6/24.
+//
+
+#import <Foundation/Foundation.h>
+#import "WeMobNetwork.h"
+#import "WeMobBannerAdSize.h"
+
+@interface WeMobILineItem : NSObject
+
+-(NSString *)getName;
+
+-(WeMobNetwork *)getNetwork;
+
+-(int)getPriority;
+
+-(float)getEcpm;
+
+-(WeMobBannerAdSize)getBannerAdSize;
+
+-(NSDictionary *)getServerExtras;
+
+@end
