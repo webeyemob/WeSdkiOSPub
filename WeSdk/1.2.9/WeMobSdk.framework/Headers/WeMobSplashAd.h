9dea90b4f5d6d4f145ba521f73180f8f39003c0a
tamefox <tamefox@chinawebeye.com>
2019-09-17 16:30:30 +0800

    .

---
+++ WeSdk/1.2.9/WeMobSdk.framework/Headers/WeMobSplashAd.h
@@ -0,0 +1,32 @@
+//
+//  WeMobSplashAd.h
+//  WeMobSdk
+//
+//  Created by Matthew on 2019/8/15.
+//  Copyright © 2019年 WeMob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "WeMobILineItem.h"
+#import "WeMobSplashAdDelegate.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WeMobSplashAd : NSObject
+
+@property(nonatomic, strong, readonly) NSString *adUnitId;
+@property(nonatomic, weak, nullable) id<WeMobSplashAdDelegate> delegate;
+
+-(id)initWithAdUnitId:(NSString *)adUnitId uiWindow:(UIWindow *)uiWindow;
+
+-(void)setBottomView:(UIView *)bottomView;
+-(void)setWidth:(int)width height:(int)height;
+
+-(void)loadAd;
+-(BOOL)isReady;
+-(void)destroy;
+
+@end
+
+NS_ASSUME_NONNULL_END
